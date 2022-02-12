21239f93edff7fb9b7defc90caeb31aa3f3bbd3e
Diego Araújo <diesaraujo@gmail.com>
2022-02-12 10:57:42 -0300

    Remove all becaus bug of werkzeug

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from flask_restplus import Api
-
-app = Flask(__name__)
-
-api = Api(app, version='1.0', title='Api Test - Diego Santos Araujo', description='Test Api Flask PTF', prefix='/api')
--- run.py
+++
@@ -1,7 +0,0 @@
-from re import S
-from app import app
-from os import environ, environb
-
-if __name__ == '__main__':
-    SERVER_HOST = environ.get('SERVER_HOST', 'localhost')
-    app.run(host=SERVER_HOST, port=5500)
