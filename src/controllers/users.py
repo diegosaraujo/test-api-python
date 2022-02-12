d04e95a8ab94f0be15d2d73f4149986a0394a6fb
Diego Araújo <diesaraujo@gmail.com>
2022-02-12 18:24:33 -0300

    Added database and method GetAll

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -1,20 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, Response
 from flask_restx import Resource, Api, Namespace
 from src.models.user import user
+import json
 
+from flask_sqlalchemy import SQLAlchemy
 from src.config.swagger import swagger
 
 app, api = swagger.app, swagger.api
+#conn = database.app
 
 
-# Depois terá que vir do banco de dados
-users_db = [
-    {'id':1, 'name': 'Diego', 'country':'Brazil'}, 
-    {'id':2, 'name': 'Thabata', 'country':'Argentina'}
-    ]
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/ptf'
+
+db = SQLAlchemy(app)#api
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    country = db.Column(db.String(50))
+    
+    def to_json(self):
+        return {"id": self.id, "name": self.name, "country": self.country}
+        
+
 
 @api.route('/users')
 class UserList(Resource):
-    @api.marshal_list_with(user)
+    #@api.marshal_list_with(user)
     def get(self, ):
-        return users_db
+        user_database = User.query.all()
+        user_json = [user.to_json() for user in user_database]
+        return jsonify({
+            'results':user_json, 
+            'success': True,
+            'count': len(user_database)
+            })
