b9653c5e85ab7fe2d8eda1ee5a1bb627b0f12136
Diego Araújo <diesaraujo@gmail.com>
2022-02-13 08:49:32 -0300

    Create pagination in Users

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
 from flask_restx import Resource, Api, Namespace
 from src.models.user import user
 import json
@@ -19,22 +19,45 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     country = db.Column(db.String(50))
-    
-    def to_json(self):
-        return {"id": self.id, "name": self.name, "country": self.country}
-        
 
 
-@api.route('/users')
+
+@api.route('/users',methods=['GET'], defaults={"page": 1, "per_page": 100})
+@api.route('/users/<int:page>/<int:per_page>', methods=['GET'])
+
+@api.doc(params={'page': 'Page of pagination', 'per_page': 'Total items returned in pagination'})
+
 class UserList(Resource):
     #@api.marshal_list_with(user)
-    def get(self, ):
-        user_database = User.query.all()
-        user_json = [user.to_json() for user in user_database]
+    def get(self, page, per_page):
+        page = page
+        per_page = per_page
+        user_database = User.query.order_by(User.id).paginate(page=page, per_page = per_page)
+
+        data=[]
+        for user in user_database.items:
+            data.append({
+                'id': user.id,
+                'name': user.name,
+                'country': user.country
+            })
+
         
-        response = generateResponse(user_json, True)
+        meta = {
+            'page': user_database.page,
+            'total_per_pages': len(data),
+            'total_count': user_database.total,
+            'prev_page': user_database.prev_num,
+            'next_page': user_database.next_num,
+            'has_next': user_database.has_next,
+            'has_prev': user_database.has_prev
+        }
         
-        return response
+        return jsonify({
+            'results':data,
+            'success': True,
+            'meta': meta
+        })
 
 
 @api.route('/user/name/<name>')
